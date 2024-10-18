87ad4a4eebbe9835517b3939c7b88dd9f8fe7097
JoaoPedro <joaopedroredondo@hotmail.com>
2024-10-17 23:06:57 -0300

    Adicionando mensagem de saudação

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,9 +1,22 @@
 package main;
+import java.util.Scanner;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {		
-		System.out.println("Olá, Mundo!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        
+        saudacaoPersonalizada();
+    }
 
+    public static void saudacaoPersonalizada() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Bem-vindo ao Java, " + nome + "!");
+        
+        scanner.close();
+    }
 }
